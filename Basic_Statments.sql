d78dc81d2a9d90c223bb332d376de5616476e94c
Liam Jackson <ljackson3613@yahoo.com>
2020-12-17 10:33:02 -0600

    More Notes

--- Basic_Statments.sql
+++ Basic_Statments.sql
@@ -19,7 +19,23 @@ SELECT *
 FROM properties_2017
 where finishedsquarefeet12 between 1000 and 3000;
 
+#Select is also used for calculations and strings.
+SELECT 2+2;
 
+SELECT 'Hello';
+
+#Compare the following quaries, pay attention to the column name in the output
+
+#Concatenation
+Select concat (first_name, '', last_name) as full_name
+from employees; 
+
+#Notice that we can add logical operators like or/and between the column = value part of the WHERE clause.
+select *
+from employees
+where first_name = 'Georgi'
+or first_name = 'Mary'
+order by first_name #Used to sort by first_name
 
 
 
